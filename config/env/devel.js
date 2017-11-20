5887b5afca8af1eb8a89af09a3f2b94cd978fa2c
Luis Felipe <luis.felipe.almeida.nascimento@gmail.com>
2017-11-20 20:20:03 -0200

    sem db

--- config/env/devel.js
+++ config/env/devel.js
@@ -1,4 +1,4 @@
 module.exports = {
   secret: "DCC195",
-  db: process.env.MONGODB_URI|| "mongodb://admin:1234@ds115436.mlab.com:15436/trabalho3web32017"
+  db: "mongodb://admin:1234@ds115436.mlab.com:15436/trabalho3web32017"
 };
