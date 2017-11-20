5f249b9c5be7ee9d7b103f03d3d44941752566c6
Luis Felipe <luis.felipe.almeida.nascimento@gmail.com>
2017-11-20 20:15:43 -0200

    com db

--- config/env/devel.js
+++ config/env/devel.js
@@ -1,4 +1,4 @@
 module.exports = {
   secret: "DCC195",
-  db: process.env.MONGODB_URI|| "mongodb://localhost/trab32017"
+  db: process.env.MONGODB_URI|| "mongodb://admin:1234@ds115436.mlab.com:15436/trabalho3web32017"
 };
--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var express = require("./config/express");
-//var mongoose = require("./config/mongoose")
-//var db = mongoose();
+var mongoose = require("./config/mongoose")
+var db = mongoose();
 var app = express();
 var porta = process.env.PORT || 3000
 app.listen(porta);
