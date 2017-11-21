5484e37761d7842dbacbdaf1b79cab7d617356cc
Luis Felipe <luis.felipe.almeida.nascimento@gmail.com>
2017-11-20 22:09:37 -0200

    a

--- config/mongoose.js
+++ config/mongoose.js
@@ -2,7 +2,9 @@ var config = require('./config');
 var mongoose = require('mongoose');
 
 module.exports = function(){
+  console.log("Conectando a base de dados");
   mongoose.connect(config.db, {useMongoClient: true});
+  console.log("Conexao concluida");
   mongoose.Promise = global.Promise;
   require("../app/models/produto.model");
   return mongoose;
--- index.js
+++ index.js
@@ -4,6 +4,6 @@ var db = mongoose();
 var app = express();
 var porta = process.env.PORT || 3000
 app.listen(porta);
-console.log("Escutando em http://localhost:3000");
+console.log("Escutando em http://localhost:"+porta);
 
 module.exports = app;
